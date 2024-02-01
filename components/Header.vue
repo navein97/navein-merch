a5d874ff4fff6b0fe1ea80ff3280c5256c329218
Naveinthiran Kulaendran <naveinthirankulaendran@Naveinthirans-MacBook-Air.local>
2024-02-01 21:58:02 +0800

    update

---
+++ components/Header.vue
@@ -0,0 +1,31 @@
+<template>
+  <v-app-bar>
+    <v-toolbar-title @click="$router.push('/')">
+      Shopping Website
+    </v-toolbar-title>
+    <v-spacer></v-spacer>
+    <v-btn @click="$router.push('/Cart')" class="text-none" stacked>
+      <v-badge :content="cartStore.productsTotal" color="error">
+        <button>
+          <i class="material-icons mr-2">add_shopping_cart</i>
+        </button>
+      </v-badge>
+    </v-btn>
+    <v-btn
+      :prepend-icon="
+        theme === 'light' ? 'mdi-weather-sunny' : 'mdi-weather-night'
+      "
+      @click="cartStore.toggleTheme()"
+    >
+      Toggle Theme
+    </v-btn>
+  </v-app-bar>
+</template>
+
+<script setup>
+import { useCartStore } from "../stores/cart";
+const cartStore = useCartStore();
+</script>
+
+<style lang="scss" scoped>
+</style>
